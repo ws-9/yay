04567997644b215172ca25c2f08907b4d183ba27
ws-9 <93690318+ws-9@users.noreply.github.com>
2025-09-14 15:24:30 -0400

    init spring boot server

---
+++ yay_backend/src/main/java/com/ws/yay_backend/YayBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ws.yay_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YayBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YayBackendApplication.class, args);
+	}
+
+}
---
+++ yay_backend/src/test/java/com/ws/yay_backend/YayBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ws.yay_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YayBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
