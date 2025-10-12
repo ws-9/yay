97a72ef82ecf642c1b9f31955031fb1eae00c67b
ws-9 <93690318+ws-9@users.noreply.github.com>
2025-10-11 20:07:12 -0400

    reformat spaces from 4 to 2

--- yay_backend/src/test/java/com/ws/yay_backend/YayBackendApplicationTests.java
+++ yay_backend/src/test/java/com/ws/yay_backend/YayBackendApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class YayBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
