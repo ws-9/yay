c79bc70bf14e05b9f939ce08a0f17029b1f57abb
ws-9 <93690318+ws-9@users.noreply.github.com>
2025-10-19 14:28:07 -0400

    use numerical ordering sql scripts

--- db_init_scripts/02_seed_users_and_roles.sql
+++ db_init_scripts/02_seed_users_and_roles.sql
@@ -1,21 +1,3 @@
-CREATE TABLE users (
-   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-   username VARCHAR(50) UNIQUE NOT NULL,
-   password VARCHAR(100) NOT NULL,
-   enabled BOOLEAN NOT NULL
-);
-
-CREATE TABLE roles (
-   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-   name VARCHAR(50) UNIQUE NOT NULL
-);
-
-CREATE TABLE user_roles (
-    user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
-    role_id BIGINT NOT NULL REFERENCES roles(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
-    PRIMARY KEY (user_id, role_id)
-);
-
 INSERT INTO users (username, password, enabled)
 VALUES
     ('user', '{noop}password', true),
