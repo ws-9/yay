b8e6a91c5acc5661cfcf2bdede74847f9e78b986
ws-9 <93690318+ws-9@users.noreply.github.com>
2025-10-21 18:42:15 -0400

    add react router

--- yay_frontend/src/main.jsx
+++ yay_frontend/src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter } from 'react-router'
+import { RouterProvider } from 'react-router'
 import './index.css'
 import App from './App.jsx'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  }
+]);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
