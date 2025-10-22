0810302c7b6dbfd5f1a1d5d1ff9b9262ffefefa2
ws-9 <93690318+ws-9@users.noreply.github.com>
2025-10-21 20:08:30 -0400

    add basic routes

--- yay_frontend/src/main.jsx
+++ yay_frontend/src/main.jsx
@@ -4,12 +4,28 @@ import { createBrowserRouter } from 'react-router'
 import { RouterProvider } from 'react-router'
 import './index.css'
 import App from './App.jsx'
+import Home from './views/Home.jsx'
+import Register from './views/Register.jsx'
+import Login from './views/Login.jsx'
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <App />,
-  }
+    Component: App,
+    children: [
+      {
+        path: '/',
+        Component: Home,
+      },
+      {
+        path: 'register',
+        Component: Register,
+      },
+      {
+        path: 'login',
+        Component: Login
+      }
+    ]
+  },
 ]);
 
 createRoot(document.getElementById('root')).render(
