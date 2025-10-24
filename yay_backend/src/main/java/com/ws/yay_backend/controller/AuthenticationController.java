ca47154d117b218ab92f3eeaca8c30fd9a877768
ws-9 <93690318+ws-9@users.noreply.github.com>
2025-10-23 20:11:26 -0400

    add controllers swagger docs

--- yay_backend/src/main/java/com/ws/yay_backend/controller/AuthenticationController.java
+++ yay_backend/src/main/java/com/ws/yay_backend/controller/AuthenticationController.java
@@ -4,11 +4,14 @@ import com.ws.yay_backend.request.AuthenticationRequest;
 import com.ws.yay_backend.request.RegisterRequest;
 import com.ws.yay_backend.response.AuthenticationResponse;
 import com.ws.yay_backend.service.AuthenticationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Authentication Controller")
 @RestController
 @RequestMapping("/api/auth")
 public class AuthenticationController {
@@ -19,12 +22,14 @@ public class AuthenticationController {
     this.authenticationService = authenticationService;
   }
 
+  @Operation(summary = "Create a new user account")
   @PostMapping("/register")
   @ResponseStatus(HttpStatus.CREATED)
   public void register(@RequestBody @Valid RegisterRequest request) throws Exception {
     authenticationService.register(request);
   }
 
+  @Operation(summary = "Retrieve access token and user info")
   @PostMapping("/login")
   @ResponseStatus(HttpStatus.OK)
   public AuthenticationResponse login(@RequestBody @Valid AuthenticationRequest request) {
--- yay_backend/src/main/java/com/ws/yay_backend/controller/CommunityController.java
+++ yay_backend/src/main/java/com/ws/yay_backend/controller/CommunityController.java
@@ -5,6 +5,8 @@ import com.ws.yay_backend.response.GetCommunityResponse;
 import com.ws.yay_backend.response.GetMemberResponse;
 import com.ws.yay_backend.response.JoinCommunityResponse;
 import com.ws.yay_backend.service.CommunityService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Community Controller")
 @RestController
 @RequestMapping("/api/communities")
 public class CommunityController {
@@ -23,39 +26,46 @@ public class CommunityController {
     this.communityService = communityService;
   }
 
+  @Operation(summary = "Get all existing communities")
   @GetMapping
   public List<GetCommunityResponse> getAllCommunities() {
     return communityService.getAll();
   }
 
+  @Operation(summary = "Create a community")
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
   public GetCommunityResponse createCommunity(@RequestBody @Valid CreateCommunityRequest request) {
     return communityService.createCommunity(request);
   }
 
+  @Operation(summary = "Get community by id")
   @GetMapping("/{id}")
   public GetCommunityResponse getCommunity(@PathVariable @Min(value = 1) long id) {
     return communityService.getCommunity(id);
   }
 
+  @Operation(summary = "Delete community by id")
   @DeleteMapping("/{id}")
   @ResponseStatus(HttpStatus.NO_CONTENT)
   public void deleteCommunity(@PathVariable @Min(value = 1) Long id) {
     communityService.deleteCommunity(id);
   }
 
+  @Operation(summary = "Get all community members by id")
   @GetMapping("{id}/members")
   public List<GetMemberResponse> getAllMembers(@PathVariable @Min(value = 1) Long id) {
     return communityService.getAllMembers(id);
   }
 
+  @Operation(summary = "Add user to community")
   @PostMapping("{id}/members")
   @ResponseStatus(HttpStatus.CREATED)
   public JoinCommunityResponse joinCommunity(@PathVariable @Min(value = 1) Long id) {
     return communityService.joinCommunity(id);
   }
 
+  @Operation(summary = "Remove user from a community")
   @DeleteMapping("{id}/members/{userId}")
   @ResponseStatus(HttpStatus.NO_CONTENT)
   public void removeMember(@PathVariable @Min(value = 1) Long id, @PathVariable @Min(value = 1) Long userId) {
