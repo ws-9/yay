aaa1a5bb94ed4099a6678f4cc668cd44cc59003f
ws-9 <93690318+ws-9@users.noreply.github.com>
2025-11-23 01:35:23 -0500

    use @EnableWebSocketSecurity
    Bypassed CSRF with an empty interceptor.

--- yay_backend/src/main/java/com/ws/yay_backend/config/WebSocketSecurityConfig.java
+++ yay_backend/src/main/java/com/ws/yay_backend/config/WebSocketSecurityConfig.java
@@ -1,29 +1,31 @@
 package com.ws.yay_backend.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
 import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
+import org.springframework.security.config.annotation.web.socket.EnableWebSocketSecurity;
+import org.springframework.security.messaging.access.intercept.MessageMatcherDelegatingAuthorizationManager;
 
 @Configuration
-public class WebSocketSecurityConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
-  @Override
-  protected void configureInbound(final MessageSecurityMetadataSourceRegistry messages) {
-    // Customize authorization mapping
-    messages.nullDestMatcher().authenticated();
+@EnableWebSocketSecurity
+public class WebSocketSecurityConfig {
+  @Bean
+  AuthorizationManager<Message<?>> messageAuthorizationManager(MessageMatcherDelegatingAuthorizationManager.Builder messages) {
+    messages
+        .nullDestMatcher().authenticated()
+        .simpSubscribeDestMatchers("/user/**").authenticated()
+        .simpDestMatchers("/app/**").authenticated();
 
-    messages.simpSubscribeDestMatchers("/user/**").authenticated();
-    messages.simpDestMatchers("/app/**").hasRole("USER");
-
-//        .simpSubscribeDestMatchers("/user/queue/errors").permitAll()
-//        .simpDestMatchers("/app/**").hasRole("USER")
-//        .simpSubscribeDestMatchers("/user/**", "/topic/friends/*").hasRole("USER")
-//        .simpTypeMatchers(MESSAGE, SUBSCRIBE).denyAll()
-//        .anyMessage().denyAll();
+    return messages.build();
   }
 
-  // For testing purposes, you might have disabled simpSubscribeDestMatchersCSRF protection temporarily
-  @Override
-  protected boolean sameOriginDisabled() {
-    return true;
+  // Bypass CSRF
+  @Bean
+  public ChannelInterceptor csrfChannelInterceptor() {
+    return new ChannelInterceptor() {};
   }
 }
