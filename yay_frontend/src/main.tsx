cd142b6285ccafddee82bf79fceed01697a678b7
ws-9 <93690318+ws-9@users.noreply.github.com>
2025-12-07 15:18:59 -0500

    setup

--- yay_frontend/src/main.tsx
+++ yay_frontend/src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.tsx';
+import { createBrowserRouter, RouterProvider } from 'react-router';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Hello World!</div>,
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
