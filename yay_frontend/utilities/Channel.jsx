d422c1c168825c408cfba95e9234282e4a5c36d0
ws-9 <93690318+ws-9@users.noreply.github.com>
2025-12-07 03:02:54 -0500

    init typescript frontend

--- yay_frontend/utilities/Channel.jsx
+++
@@ -1,7 +0,0 @@
-export default class Channel {
-  constructor(id = 0, name = "", communityId = 0) {
-    this.id = id
-    this.name = name
-    this.communityId = communityId
-  }
-}
--- yay_frontend/utilities/User.jsx
+++
@@ -1,7 +0,0 @@
-export default class User {
-  constructor(id = 0, username = "", roles = []) {
-    this.id = id
-    this.username = username
-    this.roles = roles
-  }
-}
