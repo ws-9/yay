379eac18f15a746c9cb16aa36ea6a63f423779b6
ws-9 <93690318+ws-9@users.noreply.github.com>
2025-12-25 00:50:07 -0500

    fix websocket dates

--- yay_backend/src/main/java/com/ws/yay_backend/service/WebSocketServiceImpl.java
+++ yay_backend/src/main/java/com/ws/yay_backend/service/WebSocketServiceImpl.java
@@ -8,25 +8,30 @@ import com.ws.yay_backend.dto.event.CreateChannelMessageEvent;
 import com.ws.yay_backend.entity.Channel;
 import com.ws.yay_backend.entity.ChannelMessage;
 import com.ws.yay_backend.entity.User;
+import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class WebSocketServiceImpl implements  WebSocketService {
   private final ChannelMessageRepository channelMessageRepository;
   private final ChannelRepository channelRepository;
   private final UserRepository userRepository;
+  private final EntityManager entityManager;
 
   @Autowired
-  public WebSocketServiceImpl(ChannelMessageRepository channelMessageRepository, ChannelRepository channelRepository, UserRepository userRepository) {
+  public WebSocketServiceImpl(ChannelMessageRepository channelMessageRepository, ChannelRepository channelRepository, UserRepository userRepository, EntityManager entityManager) {
     this.channelMessageRepository = channelMessageRepository;
     this.channelRepository = channelRepository;
     this.userRepository = userRepository;
+    this.entityManager = entityManager;
   }
 
   @Override
+  @Transactional
   public ChannelMessageBroadcast createMessage(Long channelId, CreateChannelMessageEvent event) {
     if (event.message() == null || event.message().isBlank()) {
       throw new RuntimeException("Message cannot be empty");
@@ -46,6 +51,8 @@ public class WebSocketServiceImpl implements  WebSocketService {
 
     ChannelMessage message = new ChannelMessage(event.message(), user, channel);
     ChannelMessage saved = channelMessageRepository.save(message);
+    entityManager.flush();
+    entityManager.refresh(saved);
 
     return new ChannelMessageBroadcast(saved);
   }
