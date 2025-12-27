d795f2b5abee1bb27688d5ef5a10f218b749bc3c
ws-9 <93690318+ws-9@users.noreply.github.com>
2025-12-26 23:54:15 -0500

    optimize inbox scroll rerendering

--- yay_frontend/src/components/ChatPage/Inbox.tsx
+++ yay_frontend/src/components/ChatPage/Inbox.tsx
@@ -25,22 +25,19 @@ export default function Inbox({
   } = useInfChannelMessagesQuery(selectedChannel);
   const messageEvents = useChannelSubscription(selectedChannel);
   const { ref: endOfInboxRef, inView: endOfInboxInView } = useInView();
-  const scrollRef = useRef<HTMLDivElement>(null);
-  const [isScrolledToBottom, setIsScrolledToBottom] = useState(true);
+  const scrollContainerRef = useRef<ScrollableContainerHandle>(null);
   const [isInitialLoad, setIsInitialLoad] = useState(true);
 
   // defines what to forward to the parent component
   useImperativeHandle(ref, () => ({
     scrollToBottom: () => {
-      if (scrollRef.current) {
-        scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
-      }
+      scrollContainerRef.current?.scrollToBottom();
     },
   }));
 
   useEffect(() => {
-    if (scrollRef.current) {
-      scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
+    if (scrollContainerRef.current) {
+      scrollContainerRef.current.scrollToBottom();
     }
   }, []);
 
@@ -60,6 +57,65 @@ export default function Inbox({
     (a, b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime(),
   );
 
+  useEffect(() => {
+    if (!scrollContainerRef.current) {
+      return;
+    }
+
+    if (isInitialLoad && allMessages.length > 0) {
+      scrollContainerRef.current.scrollToBottom();
+      setIsInitialLoad(false);
+      return;
+    }
+
+    if (scrollContainerRef.current.isAtBottom()) {
+      scrollContainerRef.current.scrollToBottom();
+    }
+  }, [allMessages, isInitialLoad]);
+
+  const renderedMessages = allMessages.map(message => (
+    <MessageRender
+      key={message.id}
+      username={message.username}
+      message={message.message}
+      createdAt={message.createdAt}
+    />
+  ));
+
+  return (
+    <ScrollableContainer ref={scrollContainerRef}>
+      <div ref={endOfInboxRef} className="bg-amber-200">
+        End
+      </div>
+      {renderedMessages}
+    </ScrollableContainer>
+  );
+}
+
+type ScrollableContainerHandle = {
+  scrollToBottom: () => void;
+  isAtBottom: () => boolean;
+};
+
+const ScrollableContainer = ({
+  children,
+  ref,
+}: {
+  children: React.ReactNode;
+  ref?: React.Ref<ScrollableContainerHandle>;
+}) => {
+  const scrollRef = useRef<HTMLDivElement>(null);
+  const [isScrolledToBottom, setIsScrolledToBottom] = useState(true);
+
+  useImperativeHandle(ref, () => ({
+    scrollToBottom: () => {
+      if (scrollRef.current) {
+        scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
+      }
+    },
+    isAtBottom: () => isScrolledToBottom,
+  }));
+
   // Add scroll event listener
   // Updates whether user is at the bottom
   useEffect(() => {
@@ -78,42 +134,12 @@ export default function Inbox({
     return () => scrollElement.removeEventListener('scroll', handleScroll);
   }, []);
 
-  useEffect(() => {
-    if (!scrollRef.current) {
-      return;
-    }
-
-    if (isInitialLoad && allMessages.length > 0) {
-      // set the vertical scroll position to the total height of the element
-      // this has the equivalent property of scrolling to the very bottom
-      scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
-      setIsInitialLoad(false);
-      return;
-    }
-
-    if (isScrolledToBottom && allMessages.length > 0) {
-      scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
-    }
-  }, [allMessages, isScrolledToBottom, isInitialLoad]);
-
-  const renderedMessages = allMessages.map(message => (
-    <MessageRender
-      key={message.id}
-      username={message.username}
-      message={message.message}
-      createdAt={message.createdAt}
-    />
-  ));
-
   return (
     <div ref={scrollRef} className="overflow-y-auto">
-      <div ref={endOfInboxRef} className="bg-amber-200">
-        End
-      </div>
-      {renderedMessages}
+      {children}
     </div>
   );
-}
+};
 
 function MessageRender({
   username,
