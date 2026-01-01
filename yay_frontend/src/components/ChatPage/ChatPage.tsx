14f87e93f42f7b4f27687a5b9dc248996a2685a4
ws-9 <93690318+ws-9@users.noreply.github.com>
2026-01-01 15:51:15 -0500

    add partial responsible chat

--- yay_frontend/src/components/ChatPage/ChatPage.tsx
+++ yay_frontend/src/components/ChatPage/ChatPage.tsx
@@ -1,25 +1,48 @@
 import MainSidebar from './MainSidebar';
 import { useIsAuthenticated } from '../../store/authStore';
-import { useEffect } from 'react';
+import { useEffect, Activity } from 'react';
 import { WS_BROKER } from '../../constants';
 import { useWebSocketActions } from '../../store/webSocketStore';
-import { useWorkspaceRoot } from '../../store/workspaceStore';
+import { useWorkspaceRoot, useActivePane } from '../../store/workspaceStore';
 import BSPChatNodeRender from '../BSPChatNodeRender/BSPChatNodeRender';
+import useMinBreakpoint from '../../hooks/useMinBreakpoint';
 
 export default function ChatPage() {
   const isAuthenticated = useIsAuthenticated();
-  const rootNode = useWorkspaceRoot();
-
   const { connect } = useWebSocketActions();
+
   useEffect(() => {
     if (isAuthenticated) {
       connect(WS_BROKER);
     }
   }, [isAuthenticated, connect]);
+
   return (
     <div className="grid h-screen grid-cols-[1fr] grid-rows-[100dvh] sm:grid-cols-[15rem_1fr]">
       <MainSidebar />
+      <SingleActivePaneRenderer />
+      <MultiPaneRenderer />
+    </div>
+  );
+}
+
+function MultiPaneRenderer() {
+  const rootNode = useWorkspaceRoot();
+  return (
+    <div className="hidden sm:block">
       <BSPChatNodeRender node={rootNode} />
     </div>
+  );
+}
+
+function SingleActivePaneRenderer() {
+  const activePane = useActivePane();
+  const isMinSm = useMinBreakpoint('sm');
+
+  // will pause subscriptions
+  return (
+    <Activity mode={isMinSm ? 'hidden' : 'visible'}>
+      <BSPChatNodeRender node={activePane} />
+    </Activity>
   );
 }
