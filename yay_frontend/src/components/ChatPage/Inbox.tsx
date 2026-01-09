0ca9839d08ceee31e2795bea1185aa4d4505d78f
ws-9 <93690318+ws-9@users.noreply.github.com>
2026-01-08 22:41:22 -0500

    add remark-gfm and autolinks

--- yay_frontend/src/components/ChatPage/Inbox.tsx
+++ yay_frontend/src/components/ChatPage/Inbox.tsx
@@ -4,6 +4,7 @@ import { useChannelSubscription } from '../../hooks/useChannelSubscription';
 import { useInView } from 'react-intersection-observer';
 import { useEffect, useImperativeHandle, useRef, useState } from 'react';
 import Markdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 
 export type InboxHandle = {
   scrollToBottom: () => void;
@@ -156,9 +157,20 @@ function MessageRender({
     <div>
       {`${formattedDate} ${username}: `}
       <Markdown
-        allowedElements={['em', 'strong', 'p']}
+        remarkPlugins={[remarkGfm]}
+        allowedElements={['em', 'strong', 'p', 'a']}
         components={{
           p: ({ children }) => <>{children}</>, // keep inline
+          a: ({ href, children }) => (
+            <a
+              href={href}
+              className="text-blue-500 underline"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {children}
+            </a>
+          ),
         }}
         unwrapDisallowed={true} // preserve content but remove formatting
       >
