616bb013e55bc3f9ab57691d638cb86c1f000cb0
ws-9 <93690318+ws-9@users.noreply.github.com>
2026-01-11 14:05:47 -0500

    style button

--- yay_frontend/src/components/chat/MainSidebar/CommunityDialog.tsx
+++ yay_frontend/src/components/chat/MainSidebar/CommunityDialog.tsx
@@ -27,7 +27,7 @@ export default function CommunityDialog() {
 
   return (
     <Dialog.Root onOpenChange={open => open && reset()}>
-      <Dialog.Trigger className="flex h-10 items-center justify-center rounded-md border border-gray-200 bg-gray-50 px-3.5 text-base font-medium text-gray-900 select-none hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:-outline-offset-1 focus-visible:outline-blue-800 active:bg-gray-100">
+      <Dialog.Trigger className="flex h-10 w-full items-center justify-center rounded-md border border-gray-200 bg-gray-50 px-3.5 text-base font-medium text-gray-900 select-none hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:-outline-offset-1 focus-visible:outline-blue-800 active:bg-gray-100">
         +
       </Dialog.Trigger>
       <Dialog.Portal>
