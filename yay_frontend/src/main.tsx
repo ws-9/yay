7b43fe6bbce5f5f15089e37ef606495615ded22f
ws-9 <93690318+ws-9@users.noreply.github.com>
2026-01-14 02:44:08 -0500

    add toast provider

--- yay_frontend/src/main.tsx
+++ yay_frontend/src/main.tsx
@@ -8,8 +8,10 @@ import Register from './components/AuthPage/Register';
 import Login from './components/AuthPage/Login';
 import { ChatPage } from './components/chat';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Toast } from '@base-ui/react/toast';
 
 const queryClient = new QueryClient();
+const toastManager = Toast.createToastManager();
 
 const router = createBrowserRouter([
   {
@@ -31,8 +33,10 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+    <Toast.Provider toastManager={toastManager}>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </Toast.Provider>
   </StrictMode>,
 );
